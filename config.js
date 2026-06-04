0a9ddc9d6caa91a901ee0bfac896259cdbe4bf78
lc-better <781023808@qq.com>
2026-06-04 10:31:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.__APP_CONFIG__ = {
   baiduMapAk: "sXh83zqRBMtLnwR0av2pRjSsr9n7cZJY",
   coordType: "bd09ll",
+  apiBaseUrl: "https://1253759588-dbq8wudl09.ap-guangzhou.tencentscf.com",
 };
 
